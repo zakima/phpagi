48076a0893654640023d55c0312425453f89d435
Dmitry V. Alexeev <mail@alexeevdv.ru>
2017-02-21 10:51:56 +0300

    One should use getenv instead of $_ENV

--- phpagi.php
+++ phpagi.php
@@ -1689,8 +1689,7 @@ class AGI
     */
     function which($cmd, $checkpath=NULL)
     {
-        global $_ENV;
-        $chpath = is_null($checkpath) ? $_ENV['PATH'] : $checkpath;
+        $chpath = is_null($checkpath) ? getenv('PATH') : $checkpath;
 
         foreach(explode(':', $chpath) as $path)
           if(is_executable("$path/$cmd"))

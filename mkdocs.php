2f6af9903267c797b0c3c1d5ff37757720aef4f1
Marco De Carli <zakima@zkm.it>
2021-09-13 10:45:20 +0200

    fix .ignore

--- mkdocs.php
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env php
-<?php
-  $cwd = getcwd();
-  passthru("phpdoc -t \"$cwd/api-docs\" -f \"$cwd/phpagi.php,$cwd/phpagi-asmanager.php,$cwd/phpagi-fastagi.php\" -pp on -ti \"phpAGI\" -o HTML:frames:earthli -s on");
-?>
-
-
